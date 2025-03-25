9fbef75530c47094ebebfe581f1c25d1fa078228
NSMichelJ <nunezsalmeronm@gmail.com>
2025-03-24 20:01:46 -0400

    Update astrojs dependency to version 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'static',
   integrations: [tailwind(), icon({
     include: {
       mdi: ['linkedin', 'twitter', 'github', 'arrow-down-bold-circle', 'arrow-right-bold-circle', 'code-block-braces', 'email-arrow-right-outline', 'briefcase', 'account', 'link-variant', 'file-document', 'email-edit-outline', 'whatsapp', 'robot-confused-outline']
