e0f6e8ed96df1ce031147d77de8e1d5741616d3f
NSMichelJ <nunezsalmeronm@gmail.com>
2025-03-24 20:33:44 -0400

    Update tailwindcss dependency to version 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
 import vercel from "@astrojs/vercel";
@@ -7,7 +7,10 @@ import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  integrations: [tailwind(), icon({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [icon({
     include: {
       mdi: ['linkedin', 'twitter', 'github', 'arrow-down-bold-circle', 'arrow-right-bold-circle', 'code-block-braces', 'email-arrow-right-outline', 'briefcase', 'account', 'link-variant', 'file-document', 'email-edit-outline', 'whatsapp', 'robot-confused-outline']
     }
